01e297f64794325de528f88e1b7d7fb2478020f2
adil-sansey <adil.sansey@gmail.com>
2021-03-26 01:37:57 +0600

    feat: add dream-team function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+  if (!Array.isArray(members)) return false;
+  let arr = [];
+  for (let value of members) {
+    if (typeof (value) == 'string') arr.push(value.trim());
+  }
+
+  return arr
+    .map(e => {
+      return e[0]
+        .toUpperCase()
+    })
+    .sort()
+    .join('');
 };
