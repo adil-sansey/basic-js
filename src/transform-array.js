e46b1864f2aa0b7b45267b232fab96dd07614a3e
adil-sansey <adil.sansey@gmail.com>
2021-03-26 06:40:17 +0600

    fix: fixed transform-array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,32 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  //throw new CustomError('Not implemented');
-  // remove line with error and write your code here
   if (!Array.isArray(arr)) throw new Error('Not an array!');
+
   let newArr = arr.slice();
   let result = [];
-  let controlSequences = ['empty', '--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+
   for (let i = 0; i < newArr.length; i++) {
+
+    if (newArr[i] == 'empty') continue;
+
     if (newArr[i] == '--discard-next')
-      if (newArr[i + 1] && !controlSequences.includes(newArr[i + 1])) {
+      if (i != newArr.length - 1 && newArr[i + 1] != 'empty') {
         newArr[i + 1] = 'empty';
         continue;
       }
 
     if (newArr[i] == '--discard-prev')
-      if (newArr[i - 1] && !controlSequences.includes(newArr[i - 1])) {
+      if (i != 0 && newArr[i + 1] != 'empty') {
         newArr[i - 1] = 'empty';
         continue;
       }
 
     if (newArr[i] == '--double-next')
-      if (newArr[i + 1] && !controlSequences.includes(newArr[i + 1]))
+      if (i != newArr.length - 1 && newArr[i + 1] != 'empty')
         newArr[i] = newArr[i + 1];
 
 
     if (newArr[i] == '--double-prev')
-      if (newArr[i - 1] && !controlSequences.includes(newArr[i - 1]))
+      if (i != 0 && newArr[i + 1] != 'empty')
         newArr[i] = newArr[i - 1];
 
   }
